8b5a66e5203ded7166e4360d1ba8a94607b2159d
ilopezl05 <66643661+ilopezl05@users.noreply.github.com>
2020-07-26 16:47:08 -0500

    JS file added

---
+++ app.js
@@ -0,0 +1,5 @@
+//Clears content of input fields.
+    function clear() {
+        document.getElementsById('reset').value = "";
+        document.getElementById('submit-msg').style.display = "inline-block";
+    }
