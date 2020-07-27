e613a134a71f2e05883bc8e80ac993d3aab8017a
ilopezl05 <66643661+ilopezl05@users.noreply.github.com>
2020-07-26 19:36:30 -0500

    Submit button not working right.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Clears content of input fields.
-    function clear() {
+    function clearFields() {
         document.getElementsByClassName('textbox').value = "";
         document.getElementById('submit-msg').style.display = "inline-block";
+        console.log("hello")
     }
