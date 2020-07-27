1fa3a8def59f965cf5396215d4600e7940aa8858
ilopezl05 <66643661+ilopezl05@users.noreply.github.com>
2020-07-26 22:45:18 -0500

    jQuery working

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ console.log("Loaded Up");
 
 //Clears content of input fields.
 
-      $('.send-btn').click(function() {
-          if ( $('.textbox').val() == '' ) {
-            $('#submit-msg').html("You need to fill all fields.")
-          }else {
-            $('#submit-msg').html("We will get back to you as soon as possible.")  
-          }
-         console.log("All done")   
-      });
+    $('.send-btn').click(function() {
+        if ( $('.textbox').val() == '' ) {
+          $('#submit-msg').html("You need to fill all fields.")
+          $('.textbox').val('')
+          $('.msg').val('')
+        }else {
+          $('#submit-msg').html("We will get back to you as soon as possible.")
+          $('.textbox').val('')
+          $('.msg').val('')   
+        }
+        
+    console.log("All done")   
+    });
